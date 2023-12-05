5002941ccedb6ba326a4cb9f987197d7d176af32
Johan Sebastian Almeida Rincon <72826930+MetadHInd@users.noreply.github.com>
2023-12-05 17:42:52 -0500

    Added examples (2 examples)
    One example is from a nightclub and the other from a attendance check at a school.

--- Concepts 2/Concepts2.js
+++ Concepts 2/Concepts2.js
@@ -83,9 +83,68 @@
  * In this case, it skips the entire position 1 of the
  * parent 'for' loop, so the generalFood array is not iterated.  
  * 
+ * 
+ * 
+ * EXAMPLE #1: In this code, it checks if someone is of legal age 
+ * to enter a party, and if it's between 2 AM and 7 AM, they get in for free:
  */
 
-//EXAMPLE:
+/** 
+let free = false;
+let hour = Math.round(Math.random() * 24);
 
+const partyEntry = () => {
+    // Ask the user for their age
+    let age = prompt("How old are you?");
+    if (age >= 18) {
+        // Check if the current hour is between 2 AM and 7 AM and entry is not already free
+        if (hour >= 2 && hour <= 7 && !free) {
+            document.write(`<p>It's ${hour} AM. You can enter for free!<p>`);
+        } else {
+            document.write(`<p>You need to pay the entrance fee because it's ${hour} AM, and it's not within the 2 AM to 7 AM range.<p>`);
+        }
+    } else {
+        document.write("<p>Sorry, you can't enter because you're not of legal age.</p>");
+    }
+}
 
+partyEntry();
+*/
+
+/**
+ * EXAMPLE #2:
+ * In this example, we have a classroom where attendance is taken using prompts,
+ * and if a student has more than 18 absences, they fail the course.
+ */
+
+let numberOfStudents = prompt("How many students are there?");
+let studentsAndAttendances = [];
+
+for (let i = 0; i < numberOfStudents; i++) {
+    studentsAndAttendances[i] = [prompt("What's your name?"), 0];
+}
+
+const takeAttendance = (name, index) => {
+    let presence = prompt("Is " + name + " present? (Y/N)");
+    if (presence.toLowerCase() === "y") {
+        studentsAndAttendances[index][1]++;
+    }
+}
+
+for (let day = 0; day < 20; day++) {
+    for (let student in studentsAndAttendances) {
+        takeAttendance(studentsAndAttendances[student][0], student);
+    }
+}
+
+for (let student in studentsAndAttendances) {
+    let result = `${studentsAndAttendances[student][0]}: <br>
+    Attendances: ${studentsAndAttendances[student][1]}<br>`;
+    document.write(result);
+    if (studentsAndAttendances[student][1] < 18) {
+        document.write("<b style='color: red'> FAILED </b><br><br>")
+    } else {
+        document.write("<b style='color: green'> PASSED </b><br><br>")
+    }
+}
 
